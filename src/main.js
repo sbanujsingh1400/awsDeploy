7d1b0574bb87c2ce55f6b2c7b42824187407d443
sbanujsingh1400 <87994121+sbanujsingh1400@users.noreply.github.com>
2025-12-25 20:24:03 +0530

    changes

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/',(req,res,next)=>{
-    
+    return res.status(200).send("Hurray! I have successfully deployed this application on AWS.");
 })
 
 app.listen(443,()=>{
